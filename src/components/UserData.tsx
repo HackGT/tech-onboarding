1fc90e9ad64bca090b1b2de688c9c13765645b41
ashishp04 <ashish.panch2@gmail.com>
2023-11-21 23:38:16 -0500

    Ashish Panchumarthy Onboarding Project

--- src/components/UserData.tsx
+++ src/components/UserData.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { apiUrl, Service } from "@hex-labs/core";
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { apiUrl, Service, Header, HeaderItem, Footer } from "@hex-labs/core";
+import { Button, SimpleGrid, Text } from "@chakra-ui/react";
 import axios from "axios";
 import UserCard from "./UserCard";
-
 const UserData: React.FC = () => {
-
   // The useState hook is used to store state in a functional component. The
   // first argument is the initial value of the state, and the second argument
   // is a function that can be used to update the state. The useState hook
@@ -19,13 +17,22 @@ const UserData: React.FC = () => {
   // only happen once when the component is loaded.
 
   useEffect(() => {
-
     // This is an example of an async function. The async keyword tells the
     // function to wait for the axios request to finish before continuing. This
     // is useful because we can't use the data from the request until it is
     // finished.
 
     const getUsers = async () => {
+      try {
+        console.log("Try");
+        const url = apiUrl(Service.USERS, "/users/hexlabs");
+        // there is no way to send filters to the backend route,
+        // so we have to filter client-side
+        const { data } = await axios.get(url);
+        setUsers(data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
 
       // TODO: Use the apiUrl() function to make a request to the /users endpoint of our USERS service. The first argument is the URL
       // of the request, which is created for the hexlabs api through our custom function apiUrl(), which builds the request URL based on
@@ -37,13 +44,13 @@ const UserData: React.FC = () => {
       // Postman will be your best friend here, because it's better to test out the API calls in Postman before implementing them here.
 
       // this is the endpoint you want to hit, but don't just hit it directly using axios, use the apiUrl() function to make the request
-      const URL = 'https://users.api.hexlabs.org/users/hexlabs';
+      const URL = "https://users.api.hexlabs.org/users/hexlabs";
 
       // uncomment the line below to test if you have successfully made the API call and retrieved the data. The below line takes
       // the raw request response and extracts the actual data that we need from it.
       // setUsers(data?.data?.profiles);
     };
-    document.title = "Hexlabs Users"
+    document.title = "Hexlabs Users";
     getUsers();
   }, []);
   // ^^ The empty array at the end of the useEffect hook tells React that the
@@ -51,27 +58,49 @@ const UserData: React.FC = () => {
   // run every time a variable changes, you can put that variable in the array
   // and it will run every time that variable changes.
 
+  const sortPhoneNumber = () => {
+    setUsers(
+      users?.filter((profile: any) => profile?.phoneNumber?.startsWith("470"))
+    );
+  };
 
+  const sortFirstName = () => {
+    setUsers(
+      users?.sort((b: any, a: any) => {
+        const comparison = b?.name?.first?.localeCompare(a?.name?.first);
+        return comparison !== 0
+          ? comparison
+          : b?.name?.first?.localeCompare(a?.name?.first);
+      })
+    );
+  };
   // TODO: Create a function that sorts the users array based on the first name of the users. Then, create a button that
   // calls this function and sorts the users alphabetically by first name. You can use the built in sort() function to do this.
-
-
+  console.log(users);
   return (
     <>
+      <Header
+        rightItem={<HeaderItem>Sign Out</HeaderItem>}
+        rightItemMobile={<HeaderItem>Sign Out</HeaderItem>}
+      >
+        <HeaderItem>Home</HeaderItem>
+        <HeaderItem>Profile</HeaderItem>
+      </Header>
       <Text fontSize="4xl">Hexlabs Users</Text>
-      <Text fontSize="2xl">This is an example of a page that makes an API call to the Hexlabs API to get a list of users.</Text>
-
+      <Text fontSize="2xl">
+        This is an example of a page that makes an API call to the Hexlabs API
+        to get a list of users.
+      </Text>
+      <Button onClick={sortPhoneNumber}>Sort Phone Number</Button>
+      <br></br>
+      <Button onClick={sortFirstName}>Sort First Name</Button>
 
       <SimpleGrid columns={[2, 3, 5]} spacing={6} padding={10}>
-
-        {/* Here we are mapping every entry in our users array to a unique UserCard component, each with the unique respective
-        data of each unique user in our array. This is a really important concept that we use a lot so be sure to familiarize
-        yourself with the syntax - compartmentalizing code makes your work so much more readable. */}
-        { users.map((user) => (
-          <UserCard user={user} />
+        {users.map((user) => (
+          <UserCard key={user.userId} user={user} />
         ))}
-
       </SimpleGrid>
+      <Footer />
     </>
   );
 };
